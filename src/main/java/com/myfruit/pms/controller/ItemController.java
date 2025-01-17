1b9da8b89922c5d7226c8c6c199be15e43bfe0c3
seohyunne <seohyun0012@naver.com>
2025-01-17 12:17:19 +0900

    상세보기 예외 처리

--- src/main/java/com/myfruit/pms/controller/ItemController.java
+++ src/main/java/com/myfruit/pms/controller/ItemController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/items")
 public class ItemController {
@@ -29,22 +31,34 @@ public class ItemController {
 
     @GetMapping("/{id}")
     public String getItem(@PathVariable("id") int id, Model model) {
-        ItemDto itemDto = itemService.getItem(id);
-        model.addAttribute("item", itemDto);
+        try{
+            ItemDto itemDto = itemService.getItem(id);
+            model.addAttribute("item", itemDto);
+        }catch(IllegalStateException e){
+            model.addAttribute("message",e.getMessage());
+            return "common/error/404";
+        }
         return "shop/detail";
     }
 
+
+
+    
+    
     // 생성페이지 GET /items/create
     // 생성 POST /items
     // 상세보기 GET /items/{id}
     // 수정 POST /items/{id}
     // 삭제 GET /items/delete/{id}
-
+    // 목록보기 GET /items
+    
     // 생성페이지 GET /items/create
     // 생성 POST /items
     // 상세보기 GET /items/{id}
     // 수정 PUT /items/{id}
     // 삭제 DELETE /items/delete/{id}
+    // 목록보기 GET /items
+
 
 
 
