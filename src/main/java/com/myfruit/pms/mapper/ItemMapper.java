2c42754ee71052adfd4f9ae5d4280d8135b5c6cc
seohyunne <seohyun0012@naver.com>
2025-01-17 11:34:26 +0900

    서비스 구현

--- src/main/java/com/myfruit/pms/mapper/ItemMapper.java
+++ src/main/java/com/myfruit/pms/mapper/ItemMapper.java
@@ -3,9 +3,20 @@ package com.myfruit.pms.mapper;
 import com.myfruit.pms.dto.ItemDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Optional;
+
 @Mapper
 public interface ItemMapper {
 
-    ItemDto getItemById(int id);
+    // Optional : 데이터 null 값인 것 방지
+    Optional<ItemDto> getItemById(int id);
+
+    //  ItemDto insertItem(ItemDto itemDto);  // PK 가 생성되었을 때 참조 주소 반환
+    //  int insertItem(ItemDto itemDto);  // PK 가 생성되었을 때 int 는 성공 or 실패 여부
     void insertItem(ItemDto itemDto);
+    List<ItemDto> getItems();
+    void updateItem(ItemDto itemDto);
+    void deleteItem(int id);
+
 }
--- src/main/java/com/myfruit/pms/service/ItemService.java
+++ src/main/java/com/myfruit/pms/service/ItemService.java
@@ -5,13 +5,36 @@ import com.myfruit.pms.mapper.ItemMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ItemService {
 
     @Autowired
     private ItemMapper itemMapper;
 
-    public ItemDto getItem(int id){
-        return itemMapper.getItemById(id);
+    public void createItem(ItemDto itemDto){
+        itemMapper.insertItem(itemDto);
     }
+
+    public ItemDto getItem(int id){
+        // NullException 처리
+        return itemMapper.getItemById(id).orElseThrow(
+                () -> new IllegalStateException("파일을 찾을 수 없습니다.")
+        );
+    }
+
+    public List<ItemDto> getItems(){
+        return itemMapper.getItems();
+    }
+
+    public void modyfiy(ItemDto itemDto){
+        itemMapper.updateItem(itemDto);
+    }
+
+    public void remove(int id){
+        itemMapper.deleteItem(id);
+    }
+
 }
